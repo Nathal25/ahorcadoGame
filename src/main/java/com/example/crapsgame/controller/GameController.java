6919eefc714a48b9cc826e846e9a4e6d4d2433a0
Nathalia Ortiz Granada <nathalia.ortiz.granada@correounivalle.edu.co>
2024-03-14 14:02:59 -0500

    Nuevos cambios Tarea agregada

--- src/main/java/com/example/crapsgame/controller/GameController.java
+++ src/main/java/com/example/crapsgame/controller/GameController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 
 public class GameController {
+    /*
     @FXML
     private TextField nicknameTextField;
 
@@ -18,6 +19,41 @@ public class GameController {
         String nickName=nicknameTextField.getText();
         String msg="Welcome: "+nickName;
         salidatextarea.setText(msg);
+    }
+    @FXML
+    public void onKetNicknameTextField(KeyEvent event) {
+        String key = event.getText();
+        System.out.println(key);
+    }
+     */
+    @FXML
+    private TextField nickNameTextField1;
+
+    @FXML
+    private TextField nickNameTextField2;
+
+    @FXML
+    private TextField nickNameTextField3;
+
+    @FXML
+    private TextField nickNameTextField4;
+
+    @FXML
+    private TextArea textArea;
+    String secretWord="vida";
+    @FXML
+    public void onHandleButtonPlay(ActionEvent event) {
+        String letter0=nickNameTextField1.getText();
+        String letter1=nickNameTextField2.getText();
+        String letter2=nickNameTextField3.getText();
+        String letter3=nickNameTextField4.getText();
+        String finalWord=letter0.concat(letter1.concat(letter2.concat(letter3)));
+        textArea.setText(finalWord);
+        if(finalWord.equals(secretWord)){
+            textArea.setText("Felicidades, haz encontrado la palabra secreta!");
+        }else {
+            textArea.setText("Sigue intentando");
+        }
     }
     @FXML
     public void onKetNicknameTextField(KeyEvent event) {
