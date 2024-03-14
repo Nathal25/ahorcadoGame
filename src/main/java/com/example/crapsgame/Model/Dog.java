a2a5034c11538a8814f2531babdd12932e530f21
Nathalia Ortiz Granada <nathalia.ortiz.granada@correounivalle.edu.co>
2024-03-13 22:47:16 -0500

    Cambios de prueba

--- src/main/java/com/example/crapsgame/Model/Dog.java
+++
@@ -1,8 +0,0 @@
-package com.example.crapsgame.Model;
-
-public class Dog implements IAnimal{
-    @Override
-    public void makeSound() {
-        System.out.println("The dog barks woof woof");
-    }
-}
